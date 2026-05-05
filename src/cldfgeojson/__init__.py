c9a2919ec906538c1e849bae97f6d0b72a3ca09c
Robert Forkel <xrotwang@googlemail.com>
2026-05-05 20:15:38 +0200

    bump version for development

--- src/cldfgeojson/__init__.py
+++ src/cldfgeojson/__init__.py
@@ -4,4 +4,4 @@ Handling GeoJSON data in the context of CLDF datasets.
 from .geojson import *  # noqa: F403 F401
 from .create import *  # noqa: F403 F401
 
-__version__ = '3.0.0'
+__version__ = '3.0.1.dev0'
